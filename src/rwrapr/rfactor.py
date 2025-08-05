c9e936b1cb19f61634ba018e18c6801a6ee58af1
kss2k <slupphaugkjell@gmail.com>
2025-08-05 13:15:03 +0200

    Remove `# type: ignore` from `src/rwrapr/rfactor.py`

--- src/rwrapr/rfactor.py
+++ src/rwrapr/rfactor.py
@@ -9,15 +9,15 @@ from .rattributes import get_rattributes
 from .toggle_rview import ToggleRView
 
 
-class RFactor(pd.Series):  # type: ignore
+class RFactor(pd.Series):
     def __init__(self, r_factor: ro.vectors.FactorVector):
-        super().__init__(convert_rfactor2py(r_factor))  # type: ignore
+        super().__init__(convert_rfactor2py(r_factor))
         self._rattributes = get_attributes_factor(r_factor)
 
     def to_r(self) -> ro.vectors.FactorVector | Any:
         return convert_categorical2r(self)
 
-    def to_py(self) -> pd.Series:  # type: ignore
+    def to_py(self) -> pd.Series:
         with ToggleRView(False):
             out = pd.Series(self)
         return out
@@ -27,14 +27,14 @@ def get_attributes_factor(df: vc.FactorVector) -> dict[str, Any] | None | Any:
     return get_rattributes(df, exclude=["class", "levels"])
 
 
-def convert_categorical2r(series: pd.Series) -> ro.vectors.FactorVector | Any:  # type: ignore
+def convert_categorical2r(series: pd.Series) -> ro.vectors.FactorVector | Any:
     with (ro.default_converter + pandas2ri.converter).context():
         r_factor = ro.conversion.get_conversion().py2rpy(series)
 
     return r_factor
 
 
-def convert_rfactor2py(r_factor: ro.vectors.FactorVector) -> pd.Series:  # type: ignore
+def convert_rfactor2py(r_factor: ro.vectors.FactorVector) -> pd.Series:
     with (ro.default_converter + pandas2ri.converter).context():
         series = ro.conversion.get_conversion().rpy2py(r_factor)
     return pd.Series(series)  # type: ignore
